c56895a68dc4775277515d6445c0feeeb7e617c8
Hangjin Zhang <zhanghangjin@51talk.com>
2017-08-02 19:40:17 +0800

    Play around components

--- App.js
+++ App.js
@@ -1,24 +1,133 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, Alert, FlatList, SectionList, Navigator } from 'react-native';
+import {
+  StackNavigator,
+} from 'react-navigation';
 
-export default class App extends React.Component {
+
+class HomeScreen extends React.Component {
+  static navigationOptions = {
+    title: 'Welcome',
+  };
   render() {
+    const { navigate } = this.props.navigation;
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-        
+      <Button
+        title="Go to Jane's profile"
+        onPress={() =>
+          navigate('Profile', { name: 'Jane' })
+        }
+      />
+    );
+  }
+}
+
+class ProfileScreen extends React.Component {
+  static navigationOptions = {
+    title: 'Profile',
+  };
+  render() {
+    const { navigate } = this.props.navigation;
+    return (
+      <View>
+        <Text>This is Jane's profile.</Text>
       </View>
     );
   }
 }
 
+// const App = StackNavigator({
+//   Home: { screen: HomeScreen },
+//   Profile: { screen: ProfileScreen },
+// });
+//
+// export default App
+
+export default class App extends React.Component {
+
+  Constructor() {
+
+  }
+
+  render() {
+    // return (
+    //   <View style={styles.container}>
+    //     <FlatList
+    //       data={[
+    //         {key: 'Devin'},
+    //         {key: 'Jackson'},
+    //         {key: 'James'},
+    //         {key: 'Joel'},
+    //         {key: 'John'},
+    //         {key: 'Jillian'},
+    //         {key: 'Jimmy'},
+    //         {key: 'Julie'},
+    //       ]}
+    //       renderItem={({item}) => <Text style={styles.item}>{item.key}</Text>}
+    //     />
+    //   </View>
+    // );
+
+
+    return (
+      <View style={styles.container}>
+        <SectionList
+          sections={[
+            {title: 'D', data: ['Devin']},
+            {title: 'J', data: ['Jackson', 'James', 'Jillian', 'Jimmy', 'Joel', 'John', 'Julie']},
+          ]}
+          renderItem={({item}) => <Text style={styles.item}>{item}</Text>}
+          renderSectionHeader={({section}) => <Text style={styles.sectionHeader}>{section.title}</Text>}
+        />
+      </View>
+    );
+
+    // _renderScene(route, navigator)
+    // {
+    //   if (route.id === 1) {
+    //     return <PageOne navigator={navigator}/>
+    //   } else if (route.id === 2) {
+    //     return <PageTwo navigator={navigator}/>
+    //   }
+    // }
+    //
+    //
+    // return (
+    //   <Navigator
+    //     initialRoute={{id: 1, }}
+    //     renderScene={this._renderScene} />
+    // );
+
+  }
+}
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//     padding: 30,
+//   },
+// });
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 22
   },
-});
+  item: {
+    padding: 10,
+    fontSize: 18,
+    height: 44,
+  },
+  sectionHeader: {
+    paddingTop: 2,
+    paddingLeft: 10,
+    paddingRight: 10,
+    paddingBottom: 2,
+    fontSize: 14,
+    fontWeight: 'bold',
+    backgroundColor: 'rgba(247,247,247,1.0)',
+  },
+})
