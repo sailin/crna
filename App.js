b859318d5dd7169cb3008e9fc23e662d3161ab5b
Hangjin Zhang <zhanghangjin@51talk.com>
2017-08-03 14:43:01 +0800

    Adding login -- not working

--- App.js
+++ App.js
@@ -1,62 +1,16 @@
 import React from 'react';
 import { StyleSheet, Text, View, Button, Alert, FlatList, SectionList, Navigator } from 'react-native';
-import {
-  StackNavigator,
-} from 'react-navigation';
+// import {
+//   StackNavigator,
+// } from 'react-navigation';
 
 
-class HomeScreen extends React.Component {
-  static navigationOptions = {
-    title: 'Welcome',
-  };
-  render() {
-    const { navigate } = this.props.navigation;
-    return (
-      <Button
-        title="Go to Jane's profile"
-        onPress={() =>
-          navigate('Profile', { name: 'Jane' })
-        }
-      />
-    );
-  }
-}
-
-class ProfileScreen extends React.Component {
-  static navigationOptions = {
-    title: 'Profile',
-  };
-  render() {
-    const { navigate } = this.props.navigation;
-    return (
-      <View>
-        <Text>This is Jane's profile.</Text>
-      </View>
-    );
-  }
-}
-
-// const App = StackNavigator({
-//   Home: { screen: HomeScreen },
-//   Profile: { screen: ProfileScreen },
-// });
-//
-// export default App
-
-class Login extends React.Component {
-  render() {
-    return (
-      <View> please log in</View>
-    )
-  }
-
-}
 export default class App extends React.Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {isLoggedIn: 0};
-  }
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {isLoggedIn: 0};
+  // }
 
   render() {
     if (this.state.isLoggedIn) {
@@ -80,25 +34,6 @@ export default class App extends React.Component {
         </View>
       )
     }
-
-
-
-    // _renderScene(route, navigator)
-    // {
-    //   if (route.id === 1) {
-    //     return <PageOne navigator={navigator}/>
-    //   } else if (route.id === 2) {
-    //     return <PageTwo navigator={navigator}/>
-    //   }
-    // }
-    //
-    //
-    // return (
-    //   <Navigator
-    //     initialRoute={{id: 1, }}
-    //     renderScene={this._renderScene} />
-    // );
-
   }
 }
 
